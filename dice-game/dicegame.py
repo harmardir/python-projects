673f5ec8488dc40ecd02571ab8ecdadd50a2f6d1
Harout <harmardir@gmail.com>
2022-02-21 21:38:40 +0200

    pushing the files

---
+++ dice-game/dicegame.py
@@ -0,0 +1,48 @@
+import random
+
+def main():
+    playerOne = input("Player one  Please enter your name: ")
+    print("Hello, " + playerOne + '\n')
+    playerTwo = input("player two Please enter your name: ")
+    print("Hello, " + playerTwo + '\n')
+
+    p1 = 0
+    winsPlayer1 = 0
+    p2 = 0
+    winsPlayer2 = 0
+    rounds = 1
+
+    while rounds != 4:
+        print("Round " + str(rounds))
+        p1 = roll_dice()
+        p2 = roll_dice()
+        print(playerOne + " Rolls: " + str(p1))
+        print(playerTwo + " Roll: " + str(p2))
+
+        if p1 > p2:
+            winsPlayer1 += 1
+            print(playerOne + " wins! \n")
+        elif p2 > p1:
+            winsPlayer2 +=1
+            print(playerTwo + " wins! \n")
+        else:
+            print("Draw! \n")
+        
+        rounds += 1
+
+    if winsPlayer1 > winsPlayer2:
+        print(playerOne + " Wins  Rounds Won: " + str(winsPlayer1))
+    elif winsPlayer2 > winsPlayer1:
+        print(playerTwo + " Wins  Rounds Won: " + str(winsPlayer2))
+    else:
+        print("Draw!")
+
+
+
+
+def roll_dice():
+    dice = random.randint(1, 6)
+    return dice
+
+
+main()
