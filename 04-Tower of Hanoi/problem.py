0776124911b5a86150dd876529d2ce31acdd1055
Harout Mardirossian <harmardir@users.noreply.github.com>
2023-08-01 09:23:21 +0300

    adding files

---
+++ 04-Tower of Hanoi/problem.py
@@ -0,0 +1,11 @@
+"""
+Objective and rules of the puzzle
+The objective is to move n number of disks from one tower to another following a set of rules. These rules are as follows: 
+
+1. Only one disk can be moved at a time 
+
+2. Only the upper disk of any of the towers can be moved 
+
+3. Larger disks cannot be placed over smaller disks
+
+"""
