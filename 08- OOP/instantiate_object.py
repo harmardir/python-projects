c2be41453f5abc56e9ebffeb671236cad9ee753f
Harout Mardirossian <harmardir@users.noreply.github.com>
2023-08-07 00:53:40 +0300

    adding files

---
+++ 08- OOP/instantiate_object.py
@@ -0,0 +1,6 @@
+class MyFirstClass:
+    print("Who wrote this?")
+
+    index = "Author-Book"
+
+    def hand_list(self, philosopher, book ): 
---
+++ 08- OOP/python_classes.py
@@ -0,0 +1,12 @@
+class MyClass:
+    a = 5
+
+    def hello(self):
+        print("Hello World!")
+
+
+myc = MyClass()
+
+print(MyClass.a)
+print(myc.a)
+print(myc.hello())
